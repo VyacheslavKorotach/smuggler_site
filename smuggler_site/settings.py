937c8564300f7db32bc2cb9a543c64478e6266cd
Vyacheslav Korotach <vkorotach@gmail.com>
2019-05-09 10:45:51 +0300

    settings 01

--- smuggler_site/settings.py
+++ smuggler_site/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0z+yd6-_hc%3*@_uv0%od898ev0b21(&b3qn4i7&&^zd(*e&o='
+SECRET_KEY = 'secret key :)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
